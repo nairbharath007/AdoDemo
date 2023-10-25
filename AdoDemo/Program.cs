6e4cbc180e4be5375a507cc3d686f3809b5ae462
Bharath Nair <nairbharath007@gmail.com>
2023-10-25 11:37:51 +0530

    Add project files.

---
+++ AdoDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ConfigurationManager.AppSettings["connectionString"];
+/*            Console.WriteLine(path);
+*/
+            //create a connection object
+            SqlConnection conn = new SqlConnection(path);
+
+            //open connection
+            conn.Open();
+
+            //tasks
+            Console.WriteLine("State: "+conn.State);
+            Console.WriteLine("Server Name: " + conn.DataSource);
+            Console.WriteLine("Database Name: " + conn.Database);
+            Console.WriteLine("TimeOut: " + conn.ConnectionTimeout);
+
+            //close connection
+            conn.Close();
+            Console.WriteLine("State: " + conn.State);
+        }
+    }
+}
